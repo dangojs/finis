2fda179c200c7c64430d6c425f5be991034fd811
joriewong <JieYu.Wang@cisdi.com.cn>
2020-06-28 15:04:02 +0800

    feat: add menu&menuItem  FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0}>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link</MenuItem>
+        </Menu>
         <h1>Hello world</h1>
         <Button className="custom">Button</Button>
         <Button disabled>Disabled Button</Button>
