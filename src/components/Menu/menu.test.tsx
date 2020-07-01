fa9db89f64dc02f9d9223febffc617d659432148
joriewong <JieYu.Wang@cisdi.com.cn>
2020-07-01 10:48:54 +0800

    test: SubMenu Component

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react';
+import { render, RenderResult, fireEvent, cleanup, wait } from '@testing-library/react';
 
 import Menu, { MenuProps } from './menu';
-import MenuItem from './menuItem'
+import MenuItem from './menuItem';
+import SubMenu from './subMenu';
 
 const testProps: MenuProps = {
-    defaultIndex: 0,
+    defaultIndex: '0',
     onSelect: jest.fn(),
     className: 'test'
 }
 const testVerticalProps: MenuProps = {
-    defaultIndex: 0,
+    defaultIndex: '0',
     mode: 'vertical'
 }
 const generateMenu = (props: MenuProps) => {
@@ -19,13 +20,31 @@ const generateMenu = (props: MenuProps) => {
             <MenuItem>active</MenuItem>
             <MenuItem disabled>disabled</MenuItem>
             <MenuItem>xyz</MenuItem>
+            <SubMenu title="dropdown">
+                <MenuItem>drop0</MenuItem>
+            </SubMenu>
         </Menu>
     );
+}
+const createStyleFile = () => {
+    const cssFile: string = `
+    .finis-submenu {
+        display: none;
+    }
+    .finis-submenu.menu-opened {
+        display: block;
+    }
+    `;
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = cssFile;
+    return style;
 }
 let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
 describe('test Menu and MenuItem component', () => {
     beforeEach(() => {
         wrapper = render(generateMenu(testProps));
+        wrapper.container.append(createStyleFile());
         menuElement = wrapper.getByTestId('test-menu');
         activeElement = wrapper.getByText('active');
         disabledElement = wrapper.getByText('disabled');
@@ -33,7 +52,8 @@ describe('test Menu and MenuItem component', () => {
     it('should render correct Menu and MenuItem based on default props', () => {
         expect(menuElement).toBeInTheDocument();
         expect(menuElement).toHaveClass('finis-menu test');
-        expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+        // expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+        expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
         expect(activeElement).toHaveClass('menu-item is-active');
         expect(disabledElement).toHaveClass('menu-item is-disabled');
     });
@@ -42,15 +62,29 @@ describe('test Menu and MenuItem component', () => {
         fireEvent.click(thirdItem);
         expect(thirdItem).toHaveClass('is-active');
         expect(activeElement).not.toHaveClass('is-active');
-        expect(testProps.onSelect).toHaveBeenCalledWith(2);
+        expect(testProps.onSelect).toHaveBeenCalledWith('2');
         fireEvent.click(disabledElement);
         expect(disabledElement).not.toHaveClass('is-active');
-        expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+        expect(testProps.onSelect).not.toHaveBeenCalledWith('1');
     });
     it('should render vertical mode when mode is set to vertical', () => {
-        cleanup(); 
+        cleanup();
         const wrapper = render(generateMenu(testVerticalProps));
         const menuElement = wrapper.getByTestId('test-menu');
         expect(menuElement).toHaveClass('menu-vertical');
+    });
+    it('should show dropdown items when hover on subMenu', async () => {
+        expect(wrapper.queryByText('drop0')).not.toBeVisible();
+        const dropdownElement = wrapper.getByText('dropdown');
+        fireEvent.mouseEnter(dropdownElement);
+        await wait(() => {
+            expect(wrapper.queryByText('drop0')).toBeVisible();
+        });
+        fireEvent.click(wrapper.getByText('drop0'));
+        expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+        fireEvent.mouseLeave(dropdownElement);
+        await wait(() => {
+            expect(wrapper.queryByText('drop0')).not.toBeVisible();
+        });
     });
 })
